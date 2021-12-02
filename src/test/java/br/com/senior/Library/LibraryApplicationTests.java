e9428b84001dff383fdb702bb4399bee12570804
Beatriz Maryah <beatrizmaryah16@gmail.com>
2021-12-02 17:25:19 -0300

    Adicionando arquivos iniciais

---
+++ src/test/java/br/com/senior/Library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senior.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
